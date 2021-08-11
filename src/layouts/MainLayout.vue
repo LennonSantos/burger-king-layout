63d96e2b834b430de071b1a2564284d9d014cd2a
Lennon <lennonsbueno@gmail.com>
2021-08-11 10:15:59 -0300

    init apostas

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,10 +8,10 @@
         active-color="primary"
         no-caps
       >
-        <q-tab name="mails" icon="fas fa-store" label="Inicio" />
-        <q-tab name="alarms" icon="fas fa-crown" label="Clube BK" />
-        <q-tab name="movies" icon="article" label="Pedidos" />
-        <q-tab name="plus" icon="linear_scale" label="Mais" />
+        <q-tab name="mails" icon="account_balance" label="Inicio" />
+        <q-tab name="alarms" icon="fas fa-medal" label="Ganhadores" />
+        <q-tab name="movies" icon="o_article" label="Apostas" />
+        <q-tab name="plus" icon="fas fa-ellipsis-h" label="Mais" />
       </q-tabs>
     </q-footer>
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,48 +5,26 @@
         <q-card flat class="bg-white full-width">
           <q-card-section>
             <div class="row justify-between items-center">
-              <div class="col" style="max-width: 40px">
-                <q-icon name="fas fa-crown" color="warning" size="2em" />
-              </div>
               <div class="col">
-                <span class="text-weight-bolder" style="font-size: 17px">Clube BK</span>
+                <span class="text-weight-bolder" style="font-size: 17px">
+                  18º rodada
+                  <q-badge outline color="primary" label="Prêmio R$ 500,00" />
+                </span>
               </div>
               <div class="col text-right">
-                <q-btn flat label="Ver mais" color="primary" class="text-bold" />
+                <q-btn flat label="Apostar agora" color="primary" class="text-bold" />
               </div>
             </div>
           </q-card-section>
 
+          <!-- <q-separator/> -->
+
           <q-card-section>
-            <!-- <q-stepper
-              v-model="step"
-              ref="stepper"
-              alternative-labels
-              color="primary"
-              animated
-              flat
-              class="bg-blue no-margin no-padding"
-            >
-              <q-step
-                :name="1"
-                title="Select"
-                icon="settings"
-                :done="step > 1"
-              />
-
-              <q-step
-                :name="2"
-                title="Create"
-                icon="create_new_folder"
-                :done="step > 2"
-              />
-
-              <q-step
-                :name="3"
-                title="an ad"
-                icon="add_comment"
-              />
-            </q-stepper> -->
+            <q-linear-progress stripe size="25px" :value="0.5">
+              <div class="absolute-full flex flex-center">
+                <q-badge color="white" text-color="primary" label="1 dia para encerrar" />
+              </div>
+            </q-linear-progress>
           </q-card-section>
         </q-card>
       </div>
@@ -60,8 +38,8 @@
               unelevated
               no-caps
             >
-              <q-icon left size="3em" name="lunch_dining" color="primary" class="no-margin" />
-              <div class="text-black q-py-sm text-weight-light" style="font-size: 15px">Cardapio</div>
+              <q-icon left size="3em" name="o_emoji_events" color="primary" class="no-margin" />
+              <div class="text-black q-py-sm text-weight-light" style="font-size: 15px">Campeonatos</div>
             </q-btn>
           </div>
           <div class="col-4">
@@ -71,8 +49,8 @@
               unelevated
               no-caps
             >
-              <q-icon left size="3em" name="confirmation_number" color="primary" class="no-margin" />
-              <div class="text-black q-py-sm text-weight-light" style="font-size: 15px">Cupons</div>
+              <q-icon left size="3em" name="o_table_rows" color="primary" class="no-margin material-icons-outlined" />
+              <div class="text-black q-py-sm text-weight-light" style="font-size: 15px">Tabelas</div>
             </q-btn>
           </div>
           <div class="col-4">
@@ -82,8 +60,8 @@
               unelevated
               no-caps
             >
-              <q-icon left size="3em" name="restaurant_menu" color="primary" class="no-margin" />
-              <div class="text-black q-py-sm text-weight-light" style="font-size: 15px">Restaurante</div>
+              <q-icon left size="3em" name="connected_tv" color="primary" class="no-margin" />
+              <div class="text-black q-py-sm text-weight-light" style="font-size: 15px">Ao vivo</div>
             </q-btn>
           </div>
         </div>
@@ -91,7 +69,7 @@
 
       <div class="col-12">
         <q-btn
-          label="Comprar"
+          label="Apostar"
           color="primary"
           class="full-width text-h6 text-weight-bolder"
           style="border-radius: 0; border-top-left-radius: 20px; border-top-right-radius: 20px"
