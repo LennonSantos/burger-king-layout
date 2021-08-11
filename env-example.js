703e952ac78ac81e460e69cd54cb8b042342b05f
Lennon <lennonsbueno@gmail.com>
2021-08-11 18:30:51 -0300

    configurado firebase

---
+++ env-example.js
@@ -0,0 +1,12 @@
+const producao = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
+}
+
+export const ENV = producao
---
+++ src/boot/firebase.js
@@ -0,0 +1,37 @@
+import { boot } from 'quasar/wrappers'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import { ENV } from '../../env'
+// import 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: ENV.apiKey,
+  authDomain: ENV.authDomain,
+  databaseURL: ENV.databaseURL,
+  projectId: ENV.projectId,
+  storageBucket: ENV.storageBucket,
+  messagingSenderId: ENV.messagingSenderId,
+  appId: ENV.appId,
+  measurementId: ENV.measurementId
+}
+
+firebase.initializeApp(firebaseConfig)
+
+firebase.firestore().enablePersistence()
+  .catch(function (err) {
+    if (err.code === 'failed-precondition') {
+      console.log(err.code)
+    } else if (err.code === 'unimplemented') {
+      console.log(err.code)
+    }
+  })
+
+const db = firebase.firestore()
+
+// "async" is optional;
+// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
+export default boot(async ({ app }) => {
+  app.config.globalProperties.$firebase = firebase
+  app.config.globalProperties.$db = db
+})
